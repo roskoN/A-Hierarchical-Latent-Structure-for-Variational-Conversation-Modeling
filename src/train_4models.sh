007cbd2e1a62c5f6877a2f678bfc6b8a2cde7d5a
Rostislav Nedelchev <Rostislav.Nedelchev@iais.fraunhofer.de>
2019-10-11 09:02:27 +0000

    changed running order

--- src/train_4models.sh
+++ src/train_4models.sh
@@ -2,7 +2,7 @@
 set -x
 
 source activate ano_diag
-python -u train.py --data=cornell --model=HRED
 python -u train.py --data=cornell --model=Seq2Seq
+python -u train.py --data=cornell --model=HRED
 python -u train.py --data=cornell --model=VHRED
 python -u train.py --data=cornell --model=VHCR
